29429a1ba9dc76620e69e4f4b64c5cf1b678980c
antrxc <uiantric06@gmail.com>
2025-04-14 05:50:00 +0000

    Added login controller

--- src/controllers/authentication.ts
+++ src/controllers/authentication.ts
@@ -2,6 +2,9 @@ import { createUser, getUserByEmail } from '../DB/Users';
 import express from 'express';
 import {authentication, random} from '../helpers';  
 
+
+
+//Register controller
 export const register = async (req: express.Request, res: express.Response)=>{
     try{
         const {email, password, username} = req.body ;
@@ -31,4 +34,40 @@ export const register = async (req: express.Request, res: express.Response)=>{
         return res.sendStatus(400);
     }
 
+}
+
+
+
+
+//Login Controller
+export const login = async (req: express.Request, res:express.Response )=> {
+    try {
+        const {email, password} = req.body;
+        if (!email || !password){
+            return res.sendStatus(400);
+        }
+
+        const user = getUserByEmail(email).select('+authentication.salt +authentication.passwo');
+        if (!user){
+            return res.sendStatus(400);
+        }
+        
+        const expectedHash = authentication((await user).authentication.salt, password)
+
+        if ((await user).authentication.password != expectedHash){
+            return res.sendStatus(403);
+        }
+
+        const salt = random();
+        (await user).authentication.sessionToken = authentication(salt, (await user)._id.toString());
+        (await user).save;
+
+
+        res.cookie('ANTRIC-AUTH', (await user).authentication.sessionToken, {domain:'localhost',path:'/'});
+
+        return res.status(200).json(user).end();
+    } catch (error) {
+        console.log(error);
+        return res.sendStatus(400);
+    }
 }
